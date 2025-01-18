92c3a1a484b478c65c16c4bc149fa2b9b98fd5a8
CRIMSONCARNAGEE <sohibaorazboyeva@gmail.com>
2025-01-18 22:31:39 +0500

    Add url copy button props

--- html and css/main.js
+++ html and css/main.js
@@ -1,5 +1,9 @@
+const elUrlShortener = document.querySelector('.url-shortener')
+
 const elUrlShortenerForm = document.querySelector('.js-url-shortener-form')
-const elUrlShortenerResults = document.querySelector('..url-shortener__resaultes')
+const elUrlShortenerResults = document.querySelector('.url-shortener__resaultes')
+const elCopyUrlShorLink = document.querySelector('.js-copy-short-link-button')
+
 
 elUrlShortenerForm.addEventListener('submit', function (evt) {
   evt.preventDefault();
@@ -7,3 +11,25 @@ elUrlShortenerForm.addEventListener('submit', function (evt) {
 
   elUrlShortenerResults.classList.add('url-shortener__resaultes--open');
 })
+
+elCopyUrlShorLink.addEventListener('click', function () {
+  console.log('test')
+});
+
+if (elUrlShortener) {
+  elUrlShortener.addEventListener('click', function (evt) {
+
+    if (evt.target.matches('.js-copy-short-link-button')) {
+      // change button text
+      evt.target.textContent = 'Copied'
+
+      // change button bg color
+      evt.target.classList.add('url-shortener__copy-button-copied')
+      // time
+      setTimeout(function () {
+        evt.target.textContent = 'Copy';
+        evt.target.classList.remove('url-shortener__copy-button-copied')
+      }, 2000);
+    }
+  });
+}
